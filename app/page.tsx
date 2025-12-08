2ba13ba2279e62af953cf771e6106b263bfee46b
amysidra <amysidra@gmail.com>
2025-12-09 04:42:46 +0700

    fix login position

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <div >
         <div className="min-h-screen flex items-center">
           <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
-            <h1 className="text-4xl font-bold mb-1">Sign In</h1>
-            <p className="font-medium mb-5 text-gray-500">
-              Login ke akun
+            <h1 className="text-4xl font-medium mb-1 text-center">Sign In</h1>
+            <p className="font-medium mb-5 text-gray-500 text-center">
+              Silahkan login ke akun anda
             </p>
             <div className="py-4 text-center">
               <LoginGoogleButton />
